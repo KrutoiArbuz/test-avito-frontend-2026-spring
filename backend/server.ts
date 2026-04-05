a0bb44bf30745b26f967f8730201d2c81092af91
Maksim Masolygin <mmasolygin@yandex.ru>
2026-04-05 15:29:38 +0700

    feat: add sort by price in server

--- backend/server.ts
+++ backend/server.ts
@@ -101,12 +101,17 @@ fastify.get<ItemsGetRequest>("/items", (request) => {
                     comparisonValue =
                         new Date(item1.createdAt).valueOf() -
                         new Date(item2.createdAt).valueOf();
+                } else if (sortColumn === "price") {
+                    const price1 = item1.price ?? 0;
+                    const price2 = item2.price ?? 0;
+                    comparisonValue = price1 - price2;
                 }
 
                 return (sortDirection === "desc" ? -1 : 1) * comparisonValue;
             })
             .slice(skip, skip + limit)
             .map((item) => ({
+                id: item.id,
                 category: item.category,
                 title: item.title,
                 price: item.price,
