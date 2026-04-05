ce8c97f05cd9ee73aee5f349e99a22d47421e4af
Maksim Masolygin <mmasolygin@yandex.ru>
2026-04-06 00:50:11 +0700

    fix: server work cors problem

--- backend/server.ts
+++ backend/server.ts
@@ -20,8 +20,23 @@ fastify.use((_, __, next) =>
 );
 
 // Настройка CORS
-fastify.use((_, reply, next) => {
+fastify.use((request, reply, next) => {
     reply.setHeader("Access-Control-Allow-Origin", "*");
+    reply.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, PUT, POST, PATCH, DELETE, OPTIONS",
+    );
+    reply.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization",
+    );
+
+    if (request.method === "OPTIONS") {
+        reply.statusCode = 204;
+        reply.end();
+        return;
+    }
+
     next();
 });
 
